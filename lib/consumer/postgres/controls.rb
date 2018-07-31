c7f3849502a4caad19c1bc15b980860704346b9d
Scott Bellware <sbellware@gmail.com>
2018-07-30 19:25:53 -0500

    The Updated event is renamed to Recorded

--- lib/consumer/postgres/controls.rb
+++ lib/consumer/postgres/controls.rb
@@ -6,7 +6,7 @@ require 'consumer/postgres/controls/category'
 require 'consumer/postgres/controls/id'
 require 'consumer/postgres/controls/message_data'
 require 'consumer/postgres/controls/position'
-require 'consumer/postgres/controls/position/store/updated'
+require 'consumer/postgres/controls/position/store/recorded'
 require 'consumer/postgres/controls/position/stream/write'
 require 'consumer/postgres/controls/stream_name'
 
