91590deb920142eba5b9e6c3cfe5e237019e1895
Scott Bellware <sbellware@gmail.com>
2019-02-11 22:34:28 -0600

    Interactive test for consumer group

--- test/interactive/consumer_group/start_consumer.rb
+++ test/interactive/consumer_group/start_consumer.rb
@@ -1,13 +1,15 @@
 require_relative '../interactive_init'
 
-logger = Log.get "Correlation Test"
+logger = Log.get "Consumer Group Test"
 
-category = ENV['CATEGORY'] || 'testPostgresConsumerCorrelation'
+category = ENV['CATEGORY'] || 'testPostgresConsumerGroup'
 logger.info "Category: #{category}"
 
-correlation_cateogry = ENV['CORRELATION_CATEGORY'] || 'testCorrelation'
-logger.info "Correlation Category: #{correlation_cateogry}"
+group_size = Integer(ENV['GROUP_SIZE'] || 2)
+logger.info "Group Size: #{group_size}"
 
+group_member = Integer(ENV['GROUP_MEMBER'] || 1)
+logger.info "Group Member: #{group_member}"
 
 position_update_interval = (ENV['POSITION_UPDATE_INTERVAL'] || 10).to_i
 
@@ -23,7 +25,8 @@ Actor::Supervisor.start do
   Controls::Consumer::Example.start(
     category,
     condition: condition,
-    correlation: correlation_cateogry,
+    group_size: group_size,
+    group_member: group_member,
     position_update_interval: position_update_interval
   )
 end
