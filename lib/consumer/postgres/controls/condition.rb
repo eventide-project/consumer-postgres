1ff4e725af4a55d0cbc502f3d7d1d803c6ce3133
Scott Bellware <sbellware@gmail.com>
2019-11-05 14:39:00 -0600

    Correlation as part of the composed condition is removed

--- lib/consumer/postgres/controls/condition.rb
+++ lib/consumer/postgres/controls/condition.rb
@@ -2,12 +2,6 @@ module Consumer
   module Postgres
     module Controls
       module Condition
-        module Correlation
-          def self.example(category:)
-            "metadata->>'correlationStreamName' like '#{category}-%'"
-          end
-        end
-
         module ConsumerGroup
           def self.example(group_size:, group_member:)
             "@hash_64(stream_name) % #{group_size} = #{group_member}"
