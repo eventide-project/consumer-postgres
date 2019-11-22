c2e22f1d633701e5b5029da9d7f7e180c42dc78e
Scott Bellware <sbellware@gmail.com>
2019-11-21 21:10:16 -0600

    Obsolete tests are removed

--- test/automated/position_store/position_stream_name.rb
+++ test/automated/position_store/position_stream_name.rb
@@ -2,17 +2,56 @@ require_relative '../automated_init'
 
 context "Position Store" do
   context "Position Stream Name" do
-    category = Controls::Category.example
+    context do
+      category = Controls::Category.example
 
-    consumer = Controls::Consumer::Identifier.build(category)
+      position_store_stream_name = Consumer::Postgres::PositionStore::StreamName.position_stream_name(category)
 
-    position_store = consumer.position_store
+      control_stream_name = "#{category}:position"
 
-    identifier = Controls::Identifier.example
-    control_stream_name = "#{category}:position-#{identifier}"
+      test "Includes the position category type" do
+        assert(position_store_stream_name == control_stream_name)
+      end
+    end
 
-    test "Identifier is appended" do
-      assert(position_store.stream_name == control_stream_name)
+    context "Stream name already includes position type" do
+      category = Controls::Category::Position.example
+
+      position_store_stream_name = Consumer::Postgres::PositionStore::StreamName.position_stream_name(category)
+
+      test "The position category type appears only once" do
+        assert(position_store_stream_name == category)
+      end
+    end
+
+    context "Consumer Identifier Given" do
+      category = Controls::Category.example
+
+      identifier = 'someIdentifier'
+
+      position_stream_name = Consumer::Postgres::PositionStore::StreamName.position_stream_name(category, consumer_identifier: identifier)
+
+      control_stream_name = "#{category}:position-#{identifier}"
+
+      test "Identifier is appended" do
+        assert(position_stream_name == control_stream_name)
+      end
+    end
+
+    context "Stream name contains additional types" do
+      category = Controls::Category.example
+
+      additional_type = 'someType'
+
+      stream_name = Controls::StreamName.example(id: :none, category: category, type: additional_type)
+
+      position_stream_name = Consumer::Postgres::PositionStore::StreamName.position_stream_name(stream_name)
+
+      control_stream_name = "#{category}:#{additional_type}+position"
+
+      test do
+        assert(position_stream_name == control_stream_name)
+      end
     end
   end
 end
