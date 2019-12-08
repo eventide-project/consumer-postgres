53cd04ee2aac1d2851ea5e0622331e362c3d373c
Scott Bellware <sbellware@gmail.com>
2019-12-08 17:54:30 -0600

    Consumer group errors are tested

---
+++ test/automated/consumer_group_error.rb
@@ -0,0 +1,71 @@
+require_relative 'automated_init'
+
+context "Consumer Group Error" do
+  context "Consumer Group Size Is Less than 1" do
+    test "Is an error" do
+      assert_raises(MessageStore::Postgres::Get::Category::ConsumerGroup::Error) do
+        Actor::Supervisor.start do
+          Controls::Consumer::Example.start(
+            'someCategory',
+            group_member: 0,
+            group_size: 0
+          )
+        end
+      end
+    end
+  end
+
+  context "Consumer Group Member Is Greater than the Consumer Group Size" do
+    test "Is an error" do
+      assert_raises(MessageStore::Postgres::Get::Category::ConsumerGroup::Error) do
+        Actor::Supervisor.start do
+          Controls::Consumer::Example.start(
+            'someCategory',
+            group_member: 2,
+            group_size: 1
+          )
+        end
+      end
+    end
+  end
+
+  context "Consumer Group Member Is Less than 0" do
+    test "Is an error" do
+      assert_raises(MessageStore::Postgres::Get::Category::ConsumerGroup::Error) do
+        Actor::Supervisor.start do
+          Controls::Consumer::Example.start(
+            'someCategory',
+            group_member: -1,
+            group_size: 1
+          )
+        end
+      end
+    end
+  end
+
+  context "Consumer Group Size is Missing" do
+    test "Is an error" do
+      assert_raises(MessageStore::Postgres::Get::Category::ConsumerGroup::Error) do
+        Actor::Supervisor.start do
+          Controls::Consumer::Example.start(
+            'someCategory',
+            group_member: 0
+          )
+        end
+      end
+    end
+  end
+
+  context "Consumer Group Member is Missing" do
+    test "Is an error" do
+      assert_raises(MessageStore::Postgres::Get::Category::ConsumerGroup::Error) do
+        Actor::Supervisor.start do
+          Controls::Consumer::Example.start(
+            'someCategory',
+            group_size: 1
+          )
+        end
+      end
+    end
+  end
+end
