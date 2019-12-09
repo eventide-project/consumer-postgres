a233693b30e594effd6dd6f7d538265d77dc796e
Nathan Ladd <nathanladd@gmail.com>
2019-12-08 18:13:47 -0600

    Reference to missing control is corrected

--- test/interactive/write_messages_periodically.rb
+++ test/interactive/write_messages_periodically.rb
@@ -2,7 +2,7 @@ require_relative './interactive_init'
 
 category = ENV['CATEGORY'] || 'testPostgresConsumer'
 
-write = MessageStore::Postgres::Write.build
+write = Messaging::Postgres::Write.build
 
 period = (ENV['PERIOD'] || 300).to_i
 
@@ -11,9 +11,9 @@ logger = Log.get "Consumer Test"
 (1..4).to_a.cycle do |stream_id|
   stream_name = MessageStore::StreamName.stream_name(category, stream_id)
 
-  message_data = Controls::MessageData::Write.example
+  message = Controls::Message.example
 
-  position = write.(message_data, stream_name)
+  position = write.(message, stream_name)
 
   logger.info "Wrote message (Stream Name: #{stream_name}, Position: #{position})"
 
